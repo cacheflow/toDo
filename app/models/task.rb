073133a860490200d792b113d1e16111c565cb43
Lex Alexander <l.alexander10@gmail.com>
2015-07-06 10:12:49 -0700

    Implement login/logout, allow user to create tasks and mark completed, and allow user to search tasks. -Implemented basic search feature to allow task searching. -Implemented and refactored mark as completed code for tasks. -Created soft delete and corresponding view to show deleted tasks. -This closes #1, closes #3, closes #4, and closes #5.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,19 @@
 class Task < ActiveRecord::Base
   include SessionsHelper
   belongs_to :user
-  # before_create :sanitize_size 
   validates_presence_of :item
+  searchkick 
   before_create :set_completed_status
   acts_as_paranoid
+  
+  scope :not_completed, -> { where(completed: false) }
+  scope :completed, -> { where(completed: true)}
+  scope :not_deleted, -> {where(deleted_at: nil)}
+  scope :search_import, -> {includes (:user)}
+
   def set_completed_status 
     !self.completed = false 
   end 
+
+  
 end
