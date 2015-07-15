ab8b2dbf75892bdd6ac120b3e1f243f173aa4e87
lexalexander10 <l.alexander10@gmail.com>
2015-07-14 17:23:45 -0700

    working on testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
-  has_secure_password 
+  include RSpec::Matchers
+  has_secure_password
   has_many :tasks, class_name: 'Task'
   validates_presence_of :name, :email, :password
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  # validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates_uniqueness_of :email, :name
-  has_attached_file(:avatar, 
+  has_attached_file(:avatar,
                 :default_url => 'http://will.i.am/img/app/footer-image.png',
                 :styles => {:medium => "300x300>", :thumb => "100x100>"})
 
@@ -12,5 +13,5 @@ class User < ActiveRecord::Base
 
   def self.query(query)
     all.joins(:user).where('users.name ILIKE ? OR items.name ILIKE ?',"%#{query}%", "%#{query}%")
-  end 
+  end
 end
